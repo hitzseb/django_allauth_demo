e34441e0c110fa5a383fcd8d0514bd0d6e59094a
hitzseb <hitzseb@gmail.com>
2023-09-30 17:52:10 -0300

    better templates

--- allauth_demo/settings.py
+++ allauth_demo/settings.py
@@ -65,7 +65,13 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
